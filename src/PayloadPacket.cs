52b462248dd3786bea3d460828da6a4b59fa7cf5
Callum Sykes <28632338+clicketyclackety@users.noreply.github.com>
2023-07-23 13:51:15 -0700

    Created PayloadPacket

---
+++ src/PayloadPacket.cs
@@ -0,0 +1,20 @@
+﻿using Crash.Geometry;
+
+namespace Crash.Changes
+{
+
+	/// <summary>A Change Description</summary>
+	public sealed class PayloadPacket
+	{
+
+		/// <summary>The Initialised Data, Geometry, BIM Objects etc.</summary>
+		public string Payload { get; set; }
+
+		/// <summary>The sum of all Transforms </summary>
+		public CTransform Transform { get; set; }
+
+		/// <summary>The latest updates to the Payload</summary>
+		public Dictionary<string, string> Updates { get; set; }
+
+	}
+}
