4915b47410810fd9a3734d906cc6b07f3522a5d0
ClicketyClackety <callumsykes@protonmail.com>
2023-08-26 11:41:00 -0700

    Updated CombineActions to be more efficient

--- src/Utils/Changes.cs
+++ src/Utils/Changes.cs
@@ -3,45 +3,27 @@ namespace Crash.Changes.Utils
 	/// <summary>Utilities related to <see cref="IChange" /></summary>
 	public static class Changes
 	{
-		public static ChangeAction combineActions(ChangeAction left, ChangeAction right)
+		public static ChangeAction CombineActions(ChangeAction left, ChangeAction right)
 		{
-			ChangeAction result = left;
+			ChangeAction result = left | right;
 
 			if (right.HasFlag(ChangeAction.Add))
 			{
 				result &= ~ChangeAction.Remove;
-				result |= ChangeAction.Add;
 			}
 			else if (right.HasFlag(ChangeAction.Remove))
 			{
-				result |= ChangeAction.Remove;
 				result &= ~ChangeAction.Add;
-			}
-
-			if (right.HasFlag(ChangeAction.Update))
-			{
-				result |= ChangeAction.Update;
-			}
-
-			if (right.HasFlag(ChangeAction.Transform))
-			{
-				result |= ChangeAction.Transform;
 			}
 
 			if (right.HasFlag(ChangeAction.Locked))
 			{
 				result &= ~ChangeAction.Unlocked;
-				result |= ChangeAction.Locked;
-			}
-			else if (right.HasFlag(ChangeAction.Unlocked))
-			{
-				result &= ~ChangeAction.Locked;
-				result |= ChangeAction.Unlocked;
 			}
 
-			if (right.HasFlag(ChangeAction.Temporary))
+			if (right.HasFlag(ChangeAction.Unlocked))
 			{
-				result |= ChangeAction.Temporary;
+				result &= ~ChangeAction.Locked;
 			}
 
 			return result;
