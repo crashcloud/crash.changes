b7009f24ef9350cdb3dd7b5fced94cd8775d76b8
Callum Sykes <callumsykes@protonmail.com>
2024-02-17 16:55:14 -0800

    Improved Transform Equality and ToString

--- src/Geometry/CTransform.cs
+++ src/Geometry/CTransform.cs
@@ -1,19 +1,16 @@
-﻿namespace Crash.Geometry
+﻿using System.Text;
+
+namespace Crash.Geometry
 {
 	/// <summary>A Transformation Matrix.</summary>
 	[JsonConverter(typeof(CTransformConverter))]
-	public struct CTransform : IEnumerable<double>, IEquatable<CTransform>
+	public readonly struct CTransform : IEnumerable<double>, IEquatable<CTransform>
 	{
 		/// <summary>the transform matrix</summary>
 		private readonly double[,] _transforms;
 
-		internal double[,] GetTransforms()
-		{
-			return _transforms;
-		}
-
 		/// <summary>An Empty Transform</summary>
-		public static CTransform Unset = new();
+		public static CTransform Unset { get; } = new();
 
 		/// <summary>Defaults to the identity matrix</summary>
 		public CTransform()
@@ -32,14 +29,17 @@
 			_transforms[0, 1] = m01;
 			_transforms[0, 2] = m02;
 			_transforms[0, 3] = m03;
+
 			_transforms[1, 0] = m10;
 			_transforms[1, 1] = m11;
 			_transforms[1, 2] = m12;
 			_transforms[1, 3] = m13;
+
 			_transforms[2, 0] = m20;
 			_transforms[2, 1] = m21;
 			_transforms[2, 2] = m22;
 			_transforms[2, 3] = m23;
+
 			_transforms[3, 0] = m30;
 			_transforms[3, 1] = m31;
 			_transforms[3, 2] = m32;
@@ -70,17 +70,6 @@
 			}
 		}
 
-		/// <summary>Returns a Matrix with uniform values</summary>
-		private static double[][] GetUniformMatrix(double value)
-		{
-			double[][] matrix =
-			{
-				new double[4] { value, value, value, value }, new double[4] { value, value, value, value },
-				new double[4] { value, value, value, value }, new double[4] { value, value, value, value }
-			};
-			return matrix;
-		}
-
 		/// <summary>Returns a Identity Matrix</summary>
 		private static double[,] GetIdentityMatrix()
 		{
@@ -92,10 +81,10 @@
 		public double this[int row, int column]
 		{
 			get => _transforms[row, column];
-			set => _transforms[row, column] = value;
+			private set => _transforms[row, column] = value;
 		}
 
-		public static double[] Flatten(double[,] twoDimensionalArray)
+		private static double[] Flatten(double[,] twoDimensionalArray)
 		{
 			int numRows = twoDimensionalArray.GetLength(0);
 			int numCols = twoDimensionalArray.GetLength(1);
@@ -114,25 +103,27 @@
 			return oneDimensionalArray;
 		}
 
-		/// <summary>Tests the Matrix for any NaN's or infinity numbers</summary>
+		// TODO : Ensure certain digits exist at certain locations
+		// TODO : e.g it seems that the last value should always be 1
+		/// <summary>Tests the Matrix for any NaN's, infinity numbers or otherwise invalid numbers</summary>
 		public bool IsValid()
 		{
-			double[] values = Flatten(_transforms);
-
-			// Check that the matrix is all zeros
-			if (values.All(v => v == 0))
+			for (int x = 0; x < 4; x++)
 			{
-				return true;
+				for (int y = 0; y < 4; y++)
+				{
+					double value = _transforms[x, y];
+					if (double.IsNaN(value) ||
+					    double.IsInfinity(value) ||
+					    double.MaxValue == value ||
+					    double.MinValue == value)
+					{
+						return false;
+					}
+				}
 			}
 
-			return values.Length == 16 &&
-			       !values.Any(v =>
-			       {
-				       return double.IsNaN(v) ||
-				              double.IsInfinity(v) ||
-				              double.MaxValue == v ||
-				              double.MinValue == v;
-			       });
+			return true;
 		}
 
 		/// <summary>Returns an Enumerator of all the values</summary>
@@ -179,13 +170,43 @@
 			return result;
 		}
 
+		public override bool Equals(object? obj)
+		{
+			return obj is CTransform other && Equals(other);
+		}
+
+		public override int GetHashCode()
+		{
+			HashCode hash = new();
+			hash.Add(_transforms[0, 0]);
+			hash.Add(_transforms[0, 1]);
+			hash.Add(_transforms[0, 2]);
+			hash.Add(_transforms[0, 3]);
+			hash.Add(_transforms[1, 0]);
+			hash.Add(_transforms[1, 1]);
+			hash.Add(_transforms[1, 2]);
+			hash.Add(_transforms[1, 3]);
+			hash.Add(_transforms[2, 0]);
+			hash.Add(_transforms[2, 1]);
+			hash.Add(_transforms[2, 2]);
+			hash.Add(_transforms[2, 3]);
+			hash.Add(_transforms[3, 0]);
+			hash.Add(_transforms[3, 1]);
+			hash.Add(_transforms[3, 2]);
+			hash.Add(_transforms[3, 3]);
+
+			return hash.GetHashCode();
+		}
+
 		public bool Equals(CTransform other)
 		{
 			for (int row = 0; row < 4; row++)
 			{
 				for (int col = 0; col < 4; col++)
 				{
-					if (Math.Abs(this[row, col] - other[row, col]) > 0.00001)
+					double thisValue = this[row, col];
+					double otherValue = other[row, col];
+					if (Math.Abs(thisValue - otherValue) > 0.00001)
 					{
 						return false;
 					}
@@ -193,6 +214,30 @@
 			}
 
 			return true;
+		}
+
+		public static bool operator ==(CTransform left, CTransform right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(CTransform left, CTransform right)
+		{
+			return !(left == right);
+		}
+
+		public override string ToString()
+		{
+			StringBuilder builder = new(120);
+			builder.Append(nameof(CTransform));
+
+			builder.Append(
+				$" : [[{_transforms[0, 0]}, {_transforms[0, 1]}, {_transforms[0, 2]}, {_transforms[0, 3]}], ");
+			builder.Append($"[{_transforms[1, 0]}, {_transforms[1, 1]}, {_transforms[1, 2]}, {_transforms[1, 3]}], ");
+			builder.Append($"[{_transforms[2, 0]}, {_transforms[2, 1]}, {_transforms[2, 2]}, {_transforms[2, 3]}],");
+			builder.Append($"[{_transforms[3, 0]}, {_transforms[3, 1]}, {_transforms[3, 2]}, {_transforms[3, 3]}]]");
+
+			return builder.ToString();
 		}
 	}
 }
--- tests/Crash.Changes.Tests/Geometry/CTransformTests.cs
+++ tests/Crash.Changes.Tests/Geometry/CTransformTests.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Crash.Changes.Tests.Geometry
+﻿namespace Crash.Changes.Tests.Geometry
 {
 	public sealed class CTransformTests
 	{
-		[TestCase(1,0,0)]
+		private const double MIN = -50.99;
+		private const double MID = 0.0;
+		private const double MAX = 5000.123;
+
+		[TestCase(1, 0, 0)]
 		[TestCase(1, 1, 0)]
 		[TestCase(1, 1, 1)]
 		[TestCase(1, 0, 1)]
@@ -18,18 +16,12 @@ namespace Crash.Changes.Tests.Geometry
 		[TestCase(0, 0, 0)]
 		public void TransformTranslation(double x, double y, double z)
 		{
-			CTransform transform = new CTransform();
-			CTransform secondTransform = new CTransform(m00:1,m11:1,m22:1,m33:1 ,m03:x,m13:y,m23:z);
+			CTransform transform = new();
+			CTransform secondTransform = new(1, m11: 1, m22: 1, m33: 1, m03: x, m13: y, m23: z);
 
 			CTransform combined = CTransform.Combine(transform, secondTransform);
 
-			double[,] expected = new double[4, 4]
-			{
-				{ 1, 0, 0, x },
-				{ 0, 1, 0, y },
-				{ 0, 0, 1, z },
-				{ 0, 0, 0, 1 }
-			};
+			double[,] expected = new double[4, 4] { { 1, 0, 0, x }, { 0, 1, 0, y }, { 0, 0, 1, z }, { 0, 0, 0, 1 } };
 
 			Assert.That(combined, Is.EqualTo(expected));
 		}
@@ -42,13 +34,13 @@ namespace Crash.Changes.Tests.Geometry
 		[TestCase(0, 1, 1)]
 		[TestCase(0, 1, 0)]
 		[TestCase(0, 0, 0)]
-		public void TransformRotation(double rx, double ry, double rz) 
+		public void TransformRotation(double rx, double ry, double rz)
 		{
-			CTransform transform = new CTransform();
-			CTransform secondTransform = new CTransform(
-				m00: Math.Cos(ry) + Math.Cos(rz), 
-				m01: -Math.Sin(rz), 
-				m02: Math.Sin(ry),
+			CTransform transform = new();
+			CTransform secondTransform = new(
+				Math.Cos(ry) + Math.Cos(rz),
+				-Math.Sin(rz),
+				Math.Sin(ry),
 				m10: Math.Sin(rz),
 				m11: Math.Cos(rx) + Math.Cos(rz),
 				m12: -Math.Sin(rx),
@@ -60,10 +52,9 @@ namespace Crash.Changes.Tests.Geometry
 
 			double[,] expected = new double[4, 4]
 			{
-				{ Math.Cos(ry)+Math.Cos(rz), -Math.Sin(rz), Math.Sin(ry), 0 },
-				{ Math.Sin(rz), Math.Cos(rx)+Math.Cos(rz), -Math.Sin(rx), 0 },
-				{ -Math.Sin(ry), Math.Sin(rx), Math.Cos(rx)+Math.Cos(ry), 0 },
-				{ 0, 0, 0, 1 }
+				{ Math.Cos(ry) + Math.Cos(rz), -Math.Sin(rz), Math.Sin(ry), 0 },
+				{ Math.Sin(rz), Math.Cos(rx) + Math.Cos(rz), -Math.Sin(rx), 0 },
+				{ -Math.Sin(ry), Math.Sin(rx), Math.Cos(rx) + Math.Cos(ry), 0 }, { 0, 0, 0, 1 }
 			};
 		}
 
@@ -75,22 +66,30 @@ namespace Crash.Changes.Tests.Geometry
 		[TestCase(0, 1, 1)]
 		[TestCase(0, 1, 0)]
 		[TestCase(0, 0, 0)]
-		public void TransformScale(double sx, double sy, double sz) 
+		public void TransformScale(double sx, double sy, double sz)
 		{
-			CTransform transform = new CTransform();
-			CTransform secondTransform = new CTransform(m00: sx, m11: sy, m22: sz);
+			CTransform transform = new();
+			CTransform secondTransform = new(sx, m11: sy, m22: sz);
 
 			CTransform combined = CTransform.Combine(transform, secondTransform);
 
-			double[,] expected = new double[4, 4]
-			{
-				{ sx, 0, 0, 0 },
-				{ 0, sy, 0, 0 },
-				{ 0, 0, sz, 0 },
-				{ 0, 0, 0, 1 }
-			};
+			double[,] expected = new double[4, 4] { { sx, 0, 0, 0 }, { 0, sy, 0, 0 }, { 0, 0, sz, 0 }, { 0, 0, 0, 1 } };
 		}
 
+		/// <summary>
+		///     Checks every single location in the matrix for equality failures
+		/// </summary>
+		[Test]
+		public void Transform_Equality()
+		{
+			for (double i = 0; i < 16; i++)
+			{
+				IEnumerable<double> zeroArray = Enumerable.Range(0, 16).Select(v => i);
+				CTransform left = new(zeroArray.ToArray());
+				CTransform right = new(zeroArray.ToArray());
 
+				Assert.That(left, Is.EqualTo(right));
+			}
+		}
 	}
 }
