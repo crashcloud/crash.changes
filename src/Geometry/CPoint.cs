7d0c8da5bade6c8fe9db3b65a20071f983dd92c8
Callum Sykes <callumsykes@protonmail.com>
2024-02-17 16:58:48 -0800

    Upped to 1.2.1 and updated csproj

--- src/Geometry/CPoint.cs
+++ src/Geometry/CPoint.cs
@@ -1,15 +1,17 @@
-﻿namespace Crash.Geometry
-{
+﻿using System.Text.Json.Serialization;
 
+namespace Crash.Geometry
+{
 	/// <summary>A 3 dimensional representation of a point.</summary>
 	[JsonConverter(typeof(CPointConverter))]
 	public struct CPoint
 	{
-
 		/// <summary>The X Coordinate</summary>
 		public double X { get; set; } = 0;
+
 		/// <summary>The Y Coordinate</summary>
 		public double Y { get; set; } = 0;
+
 		/// <summary>The Z Coordinate</summary>
 		public double Z { get; set; } = 0;
 
@@ -18,7 +20,7 @@
 		public static CPoint None => new(double.NaN, double.NaN, double.NaN);
 
 		/// <summary>Returns a Point at 0,0,0.</summary>
-		public static CPoint Origin => new(0, 0, 0);
+		public static CPoint Origin => new(0);
 
 		/// <summary>Empty Constructor</summary>
 		public CPoint() { }
@@ -31,45 +33,69 @@
 			Z = z;
 		}
 
-		/// <inheritdoc/>
+		/// <inheritdoc />
 		public override bool Equals(object? obj)
 		{
-			if (obj is not CPoint cPoint) return false;
+			if (obj is not CPoint cPoint)
+			{
+				return false;
+			}
+
 			return this == cPoint;
 		}
 
-		/// <inheritdoc/>
-		public override int GetHashCode() => X.GetHashCode() ^ Y.GetHashCode() ^ Z.GetHashCode();
+		/// <inheritdoc />
+		public override int GetHashCode()
+		{
+			return X.GetHashCode() ^ Y.GetHashCode() ^ Z.GetHashCode();
+		}
 
-		/// <inheritdoc/>
-		public override string ToString() => $"{X},{Y},{Z}";
+		/// <inheritdoc />
+		public override string ToString()
+		{
+			return $"{X},{Y},{Z}";
+		}
 
 		/// <summary>Returns a Rounded CPoint</summary>
 		/// <param name="digits">the number</param>
 		/// <returns>The number of fractional digits in the return value</returns>
-		public CPoint Round(int digits = 0) => new(Math.Round(this.X, digits),
-												   Math.Round(this.Y, digits),
-												   Math.Round(this.Z, digits));
+		public CPoint Round(int digits = 0)
+		{
+			return new CPoint(Math.Round(X, digits),
+				Math.Round(Y, digits),
+				Math.Round(Z, digits));
+		}
 
 		/// <summary>Conperts a CVector to a Cpoint</summary>
-		public static implicit operator CPoint(CVector p) => new(p.X, p.Y, p.Z);
+		public static implicit operator CPoint(CVector p)
+		{
+			return new CPoint(p.X, p.Y, p.Z);
+		}
 
 		/// <summary>Tests for mathmatic equality</summary>
 		public static bool operator ==(CPoint p1, CPoint p2)
-			=> p1.X == p2.X &&
-			p1.Y == p2.Y &&
-			p1.Z == p2.Z;
+		{
+			return p1.X == p2.X &&
+			       p1.Y == p2.Y &&
+			       p1.Z == p2.Z;
+		}
 
 		/// <summary>Tests for mathmatic inequality</summary>
-		public static bool operator !=(CPoint p1, CPoint p2) => !(p1 == p2);
+		public static bool operator !=(CPoint p1, CPoint p2)
+		{
+			return !(p1 == p2);
+		}
 
 		/// <summary>Subtracts p2 from p1</summary>
 		public static CPoint operator -(CPoint p1, CPoint p2)
-			=> new(p1.X - p2.X, p1.Y - p2.Y, p1.Z - p2.Z);
+		{
+			return new CPoint(p1.X - p2.X, p1.Y - p2.Y, p1.Z - p2.Z);
+		}
 
 		/// <summary>Adds p2 and p1</summary>
 		public static CPoint operator +(CPoint p1, CPoint p2)
-			=> new(p2.X + p1.X, p2.Y + p1.Y, p2.Z + p1.Z);
-
+		{
+			return new CPoint(p2.X + p1.X, p2.Y + p1.Y, p2.Z + p1.Z);
+		}
 	}
 }
--- src/Geometry/CVector.cs
+++ src/Geometry/CVector.cs
@@ -1,15 +1,17 @@
-﻿namespace Crash.Geometry
-{
+﻿using System.Text.Json.Serialization;
 
+namespace Crash.Geometry
+{
 	/// <summary>A 3 dimensional representation of a direction.</summary>
 	[JsonConverter(typeof(CVectorConverter))]
 	public struct CVector
 	{
-
 		/// <summary>The X Direction</summary>
 		public double X { get; set; } = 0;
+
 		/// <summary>The Y Direction</summary>
 		public double Y { get; set; } = 0;
+
 		/// <summary>The Z Direction</summary>
 		public double Z { get; set; } = 0;
 
@@ -18,7 +20,7 @@
 		public static CVector None => new(double.NaN, double.NaN, double.NaN);
 
 		/// <summary>Returns a CVector at 0,0,0.</summary>
-		public static CVector Origin => new(0, 0, 0);
+		public static CVector Origin => new(0);
 
 		/// <summary>Empty Constructor</summary>
 		public CVector() { }
@@ -31,45 +33,69 @@
 			Z = z;
 		}
 
-		/// <inheritdoc/>
+		/// <inheritdoc />
 		public override bool Equals(object? obj)
 		{
-			if (obj is not CVector cPoint) return false;
+			if (obj is not CVector cPoint)
+			{
+				return false;
+			}
+
 			return this == cPoint;
 		}
 
-		/// <inheritdoc/>
-		public override int GetHashCode() => X.GetHashCode() ^ Y.GetHashCode() ^ Z.GetHashCode();
+		/// <inheritdoc />
+		public override int GetHashCode()
+		{
+			return X.GetHashCode() ^ Y.GetHashCode() ^ Z.GetHashCode();
+		}
 
-		/// <inheritdoc/>
-		public override string ToString() => $"{X},{Y},{Z}";
+		/// <inheritdoc />
+		public override string ToString()
+		{
+			return $"{X},{Y},{Z}";
+		}
 
 		/// <summary>Returns a Rounded CVector</summary>
 		/// <param name="digits">the number</param>
 		/// <returns>The number of fractional digits in the return value</returns>
-		public CVector Round(int digits = 0) => new(Math.Round(this.X, digits),
-												   Math.Round(this.Y, digits),
-												   Math.Round(this.Z, digits));
+		public CVector Round(int digits = 0)
+		{
+			return new CVector(Math.Round(X, digits),
+				Math.Round(Y, digits),
+				Math.Round(Z, digits));
+		}
 
 		/// <summary>Conperts a CPoint to a CVector</summary>
-		public static implicit operator CVector(CPoint p) => new(p.X, p.Y, p.Z);
+		public static implicit operator CVector(CPoint p)
+		{
+			return new CVector(p.X, p.Y, p.Z);
+		}
 
 		/// <summary>Tests for mathmatic equality</summary>
 		public static bool operator ==(CVector v1, CVector v2)
-			=> v1.X == v2.X &&
-			v1.Y == v2.Y &&
-			v1.Z == v2.Z;
+		{
+			return v1.X == v2.X &&
+			       v1.Y == v2.Y &&
+			       v1.Z == v2.Z;
+		}
 
 		/// <summary>Tests for mathmatic inequality</summary>
-		public static bool operator !=(CVector v1, CVector v2) => !(v1 == v2);
+		public static bool operator !=(CVector v1, CVector v2)
+		{
+			return !(v1 == v2);
+		}
 
 		/// <summary>Subtracts p2 from p1</summary>
 		public static CVector operator -(CVector v1, CVector v2)
-			=> new(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+		{
+			return new CVector(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+		}
 
 		/// <summary>Adds p2 and p1</summary>
 		public static CVector operator +(CVector v1, CVector v2)
-			=> new(v2.X + v1.X, v2.Y + v1.Y, v2.Z + v1.Z);
-
+		{
+			return new CVector(v2.X + v1.X, v2.Y + v1.Y, v2.Z + v1.Z);
+		}
 	}
 }
